9a7f80c007658752f114110a7eade4f8b15dabc8
hohre12 <60336857+hohre12@users.noreply.github.com>
2024-09-04 00:21:02 +0900

    ADD] package, main

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { ReactNode, useCallback, useEffect, useMemo } from 'react';
 import './App.css'
+import { useRecoilState } from 'recoil';
+
+interface PrivateRouteProps {
+children: ReactNode;
+}
+
+const PrivateRoute = ({ children }: PrivateRouteProps) => {
+const isToken = useRecoilValue(tokenState);
+return isToken ? (
+    <div className="wrapper">{children}</div>
+) : (
+    <Navigate
+    replace
+    to="/login"
+    />
+);
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+    // const [token, setToken] = useRecoilState(tokenState);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+      
+    </div>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
 import App from './App.tsx'
 import './index.css'
+import { BrowserRouter as Router } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/variables.scss'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const queryClient = new QueryClient();
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
+
+root.render(
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <ThemeProvider theme={theme}>
+          {/* auto complete is not avaliable for theme */}
+          <Router>
+            <App />
+          </Router>
+        </ThemeProvider>
+      </RecoilRoot>
+    </QueryClientProvider>,
+  );
