f35fc074f0cdcee9dfdc9b58b5324b169384e295
Bae JaeWon <60336857+hohre12@users.noreply.github.com>
2024-09-10 17:09:36 +0900

    ADD] install package

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,37 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import svgr from 'vite-plugin-svgr'; // svg to react component
+import tailwindcss from 'tailwindcss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    port: 3000,
+  },
+  build: {
+    manifest: true,
+    minify: true,
+  },
+  plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@c': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@p': fileURLToPath(new URL('./src/pages', import.meta.url)),
+    },
+  },
+  // SCSS 전역 사용
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+            @use "@/styles/index.scss" as *;
+        `,
+      },
+    },
+  },
+});
