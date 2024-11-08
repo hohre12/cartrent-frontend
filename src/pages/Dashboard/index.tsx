462a83e812b7d773b49dbb08c84d4d438df11a0b
Bae JaeWon <60336857+hohre12@users.noreply.github.com>
2024-11-08 16:28:53 +0900

    UPDATE] 대시보드

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,12 +2,53 @@ import { useState } from 'react';
 import styled from 'styled-components';
 
 const Dashboard = () => {
-  const [activeGraphTab, setActiveGraphTab] = useState<
-    'customer' | 'counsel' | 'adjustment'
-  >('customer');
+  //   const [activeGraphTab, setActiveGraphTab] = useState<
+  //     'customer' | 'counsel' | 'adjustment'
+  //   >('customer');
   return (
     <DashboardWrapper>
-      <InfoWrapper>
+      <DashboardHeader>
+        <h2>대시보드</h2>
+      </DashboardHeader>
+      <DashboardContent>
+        <Box>
+          <div className="header">
+            <h3>이번달 매출현황</h3>
+          </div>
+          <div className="content"></div>
+        </Box>
+        <Box className="green">
+          <div className="header">
+            <h3>지난달 매출현황</h3>
+          </div>
+          <div className="content"></div>
+        </Box>
+        <Box>
+          <div className="header">
+            <h3>이번달 매출 1위</h3>
+          </div>
+          <div className="content"></div>
+        </Box>
+        <Box className="green">
+          <div className="header">
+            <h3>지난달 매출 1위</h3>
+          </div>
+          <div className="content"></div>
+        </Box>
+        <Box>
+          <div className="header">
+            <h3>이번달 계약1위</h3>
+          </div>
+          <div className="content"></div>
+        </Box>
+        <Box className="green">
+          <div className="header">
+            <h3>지난달 계약 1위</h3>
+          </div>
+          <div className="content"></div>
+        </Box>
+      </DashboardContent>
+      {/* <InfoWrapper>
         <LeftWrapper>
           <DashboardBoxWrapper>
             <DashboardBox className="red">
@@ -75,7 +116,7 @@ const Dashboard = () => {
             정산
           </li>
         </ul>
-      </GraphBox>
+      </GraphBox> */}
     </DashboardWrapper>
   );
 };
@@ -83,150 +124,186 @@ const Dashboard = () => {
 export default Dashboard;
 
 const DashboardWrapper = styled.div`
-  background: #eee;
+  background: #fff;
   height: 100%;
   padding: 30px;
   display: flex;
   flex-direction: column;
-  gap: 30px;
+  gap: 20px;
 `;
 
-const InfoWrapper = styled.div`
+const DashboardHeader = styled.div`
+  text-align: left;
+  h2 {
+    font-size: 24px;
+    font-weight: 700;
+  }
+`;
+const DashboardContent = styled.div`
   display: flex;
-  gap: 30px;
+  flex-wrap: wrap;
+  gap: 20px;
 `;
-
-const LeftWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 30px;
-`;
-
-const DashboardBoxWrapper = styled.div`
-  display: flex;
-  gap: 30px;
-`;
-
-const DashboardBox = styled.div`
-  width: 400px;
-  height: 200px;
-  border: 1px solid #ddd;
+const Box = styled.div`
+  width: 49%;
+  border-radius: 10px;
+  background-color: #f9f9f9;
+  padding: 20px;
+  border: 1px solid #eee;
   display: flex;
   flex-direction: column;
-  .numberWrapper {
-    background: #fff;
-    height: 80%;
+  gap: 10px;
+  &.green {
+    background-color: #e7faf2;
+  }
+  .header {
     display: flex;
-    flex-direction: column;
-    justify-content: center;
+    justify-content: space-between;
     align-items: center;
-    gap: 20px;
-    & > h2 {
-      font-size: 36px;
+    h3 {
+      font-size: 20px;
       font-weight: 700;
     }
   }
-  .textWrapper {
-    padding: 10px;
-    display: flex;
-    justify-content: space-between;
-    border-top: 1px solid #ddd;
-    align-items: center;
-    & > span {
-      width: 100px;
-      padding: 5px;
-      color: #fff;
-    }
-  }
-  &.red {
-    .numberWrapper {
-      & > h2 {
-        color: #ff3126;
-      }
-    }
-    .textWrapper {
-      & > span {
-        background: #ff3126;
-      }
-    }
-  }
-  &.purple {
-    .numberWrapper {
-      & > h2 {
-        color: #871fff;
-      }
-    }
-    .textWrapper {
-      & > span {
-        background: #871fff;
-      }
-    }
-  }
-  &.blue {
-    .numberWrapper {
-      & > h2 {
-        color: #1c24ff;
-      }
-    }
-    .textWrapper {
-      & > span {
-        background: #1c24ff;
-      }
-    }
-  }
-`;
-
-const SmallGraphWrapper = styled.div`
-  display: flex;
-  gap: 30px;
-`;
-
-const SmallGraphBox = styled.div`
-  width: 600px;
-  height: 270px;
-  background: #fff;
-  border: 1px solid #ddd;
-  border-radius: 16px;
-  text-align: left;
-  padding: 10px;
-  & > h6 {
-    color: #1aa18f;
-  }
-`;
-
-const StatusBox = styled.div`
-  width: 25%;
-  height: 500px;
-  background: #fff;
-  border: 1px solid #ddd;
-  border-radius: 16px;
-  margin-left: auto;
-  text-align: left;
-  padding: 10px;
-  & > h6 {
-    color: #1aa18f;
-  }
-`;
-
-const GraphBox = styled.div`
-  background: #fff;
-  border: 1px solid #ddd;
-  border-radius: 16px;
-  height: 500px;
-  .title {
-    text-align: left;
-    display: flex;
-    gap: 20px;
-    border-bottom: 1px solid #ddd;
-    padding: 10px;
-    li {
-      cursor: pointer;
-      &.active {
-        color: #ff3126;
-      }
-    }
-  }
   .content {
-    padding: 10px;
   }
 `;
+// const InfoWrapper = styled.div`
+//   display: flex;
+//   gap: 30px;
+// `;
+
+// const LeftWrapper = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   gap: 30px;
+// `;
+
+// const DashboardBoxWrapper = styled.div`
+//   display: flex;
+//   gap: 30px;
+// `;
+
+// const DashboardBox = styled.div`
+//   width: 400px;
+//   height: 200px;
+//   border: 1px solid #ddd;
+//   display: flex;
+//   flex-direction: column;
+//   .numberWrapper {
+//     background: #fff;
+//     height: 80%;
+//     display: flex;
+//     flex-direction: column;
+//     justify-content: center;
+//     align-items: center;
+//     gap: 20px;
+//     & > h2 {
+//       font-size: 36px;
+//       font-weight: 700;
+//     }
+//   }
+//   .textWrapper {
+//     padding: 10px;
+//     display: flex;
+//     justify-content: space-between;
+//     border-top: 1px solid #ddd;
+//     align-items: center;
+//     & > span {
+//       width: 100px;
+//       padding: 5px;
+//       color: #fff;
+//     }
+//   }
+//   &.red {
+//     .numberWrapper {
+//       & > h2 {
+//         color: #ff3126;
+//       }
+//     }
+//     .textWrapper {
+//       & > span {
+//         background: #ff3126;
+//       }
+//     }
+//   }
+//   &.purple {
+//     .numberWrapper {
+//       & > h2 {
+//         color: #871fff;
+//       }
+//     }
+//     .textWrapper {
+//       & > span {
+//         background: #871fff;
+//       }
+//     }
+//   }
+//   &.blue {
+//     .numberWrapper {
+//       & > h2 {
+//         color: #1c24ff;
+//       }
+//     }
+//     .textWrapper {
+//       & > span {
+//         background: #1c24ff;
+//       }
+//     }
+//   }
+// `;
+
+// const SmallGraphWrapper = styled.div`
+//   display: flex;
+//   gap: 30px;
+// `;
+
+// const SmallGraphBox = styled.div`
+//   width: 600px;
+//   height: 270px;
+//   background: #fff;
+//   border: 1px solid #ddd;
+//   border-radius: 16px;
+//   text-align: left;
+//   padding: 10px;
+//   & > h6 {
+//     color: #1aa18f;
+//   }
+// `;
+
+// const StatusBox = styled.div`
+//   width: 25%;
+//   height: 500px;
+//   background: #fff;
+//   border: 1px solid #ddd;
+//   border-radius: 16px;
+//   margin-left: auto;
+//   text-align: left;
+//   padding: 10px;
+//   & > h6 {
+//     color: #1aa18f;
+//   }
+// `;
+
+// const GraphBox = styled.div`
+//   background: #fff;
+//   border: 1px solid #ddd;
+//   border-radius: 16px;
+//   height: 500px;
+//   .title {
+//     text-align: left;
+//     display: flex;
+//     gap: 20px;
+//     border-bottom: 1px solid #ddd;
+//     padding: 10px;
+//     li {
+//       cursor: pointer;
+//       &.active {
+//         color: #ff3126;
+//       }
+//     }
+//   }
+//   .content {
+//     padding: 10px;
+//   }
+// `;
