4248928b92945aa275e0a80a1a2aa19dbde13be1
hohre12 <60336857+hohre12@users.noreply.github.com>
2025-01-08 23:42:04 +0900

    UPDATE] 고객목록 유비플러스 레이아웃으로 원복

---
+++ src/pages/Customer/Filter/index.tsx
@@ -0,0 +1,104 @@
+import styled from 'styled-components';
+import { textM16Regular, textS14Medium } from '@/styles/typography';
+import { SvgIcon } from '@/components/common/SvgIcon';
+import Checkbox from '@/components/checkbox/Checkbox';
+import { useState } from 'react';
+
+const CustomerFilter = () => {
+  const [isOpenFilters, setIsOpenFilters] = useState<string[]>([]);
+  const handleOpenFilters = (key: string) => {
+    if (isOpenFilters.some((it) => it === key)) {
+      const newValue = isOpenFilters.filter((it) => it !== key);
+      setIsOpenFilters(newValue);
+    } else {
+      setIsOpenFilters([...isOpenFilters, 'group']);
+    }
+  };
+  return (
+    <FilterWrapper>
+      <Filter>
+        <div
+          className="Menu"
+          onClick={() => handleOpenFilters('group')}
+        >
+          <div>
+            <SvgIcon
+              iconName="icon-arrow_up_s"
+              style={{
+                width: '20px',
+                transform: isOpenFilters.some((it) => it === 'group')
+                  ? 'rotate(90deg)'
+                  : '',
+              }}
+            />
+            <p>고객그룹</p>
+          </div>
+          <div>
+            <SvgIcon iconName="icon-search" />
+            <SvgIcon iconName="icon-plus" />
+          </div>
+        </div>
+        {isOpenFilters.some((it) => it === 'group') && (
+          <div className="SubMenu">
+            <div>
+              <Checkbox></Checkbox>
+              <p>A그룹</p>
+            </div>
+            <div>
+              <Checkbox></Checkbox>
+              <p>B그룹</p>
+            </div>
+            <div>
+              <Checkbox></Checkbox>
+              <p>C그룹</p>
+            </div>
+          </div>
+        )}
+      </Filter>
+    </FilterWrapper>
+  );
+};
+
+export default CustomerFilter;
+
+export const FilterWrapper = styled.div`
+  background: #fff;
+  width: 300px;
+  padding: 5px;
+  // 드래그 방지
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+`;
+export const Filter = styled.div`
+  text-align: left;
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  height: 270px;
+  ${textS14Medium}
+  .Menu {
+    background: #eee;
+    padding: 5px;
+    cursor: pointer;
+    display: flex;
+    justify-content: space-between;
+    & > div {
+      display: flex;
+      align-items: center;
+    }
+  }
+  .SubMenu {
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+    cursor: pointer;
+    & > div {
+      padding: 5px;
+      display: flex;
+      align-items: center;
+      gap: 5px;
+    }
+  }
+`;
